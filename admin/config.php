19d6ebc97981711057eaf8517320ad237d54414f
Maxim Zaytsev <m.zaitsev.90@gmail.com>
2018-08-07 16:04:45 +0300

    файлы с настройками 2

--- admin/config.php
+++ admin/config.php
@@ -8,18 +8,18 @@ define('HTTPS_SERVER', 'https://evadavidova.local/admin/');
 define('HTTPS_CATALOG', 'https://evadavidova.local/');
 
 // DIR
-define('DIR_APPLICATION', '/Users/mzaitsev/Workspace/shop/admin/');
-define('DIR_SYSTEM', '/Users/mzaitsev/Workspace/shop/system/');
-define('DIR_IMAGE', '/Users/mzaitsev/Workspace/shop/image/');
-define('DIR_LANGUAGE', '/Users/mzaitsev/Workspace/shop/admin/language/');
-define('DIR_TEMPLATE', '/Users/mzaitsev/Workspace/shop/admin/view/template/');
-define('DIR_CONFIG', '/Users/mzaitsev/Workspace/shop/system/config/');
-define('DIR_CACHE', '/Users/mzaitsev/Workspace/shop/system/storage/cache/');
-define('DIR_DOWNLOAD', '/Users/mzaitsev/Workspace/shop/system/storage/download/');
-define('DIR_LOGS', '/Users/mzaitsev/Workspace/shop/system/storage/logs/');
-define('DIR_MODIFICATION', '/Users/mzaitsev/Workspace/shop/system/storage/modification/');
-define('DIR_UPLOAD', '/Users/mzaitsev/Workspace/shop/system/storage/upload/');
-define('DIR_CATALOG', '/Users/mzaitsev/Workspace/shop/catalog/');
+define('DIR_APPLICATION', '/Users/mzaitsev/Workspace/evadavidova/admin/');
+define('DIR_SYSTEM', '/Users/mzaitsev/Workspace/evadavidova/system/');
+define('DIR_IMAGE', '/Users/mzaitsev/Workspace/evadavidova/image/');
+define('DIR_LANGUAGE', '/Users/mzaitsev/Workspace/evadavidova/admin/language/');
+define('DIR_TEMPLATE', '/Users/mzaitsev/Workspace/evadavidova/admin/view/template/');
+define('DIR_CONFIG', '/Users/mzaitsev/Workspace/evadavidova/system/config/');
+define('DIR_CACHE', '/Users/mzaitsev/Workspace/evadavidova/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Users/mzaitsev/Workspace/evadavidova/system/storage/download/');
+define('DIR_LOGS', '/Users/mzaitsev/Workspace/evadavidova/system/storage/logs/');
+define('DIR_MODIFICATION', '/Users/mzaitsev/Workspace/evadavidova/system/storage/modification/');
+define('DIR_UPLOAD', '/Users/mzaitsev/Workspace/evadavidova/system/storage/upload/');
+define('DIR_CATALOG', '/Users/mzaitsev/Workspace/evadavidova/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
--- config.php
+++ config.php
@@ -6,17 +6,17 @@ define('HTTP_SERVER', 'http://evadavidova.local/');
 define('HTTPS_SERVER', 'https://evadavidova.local/');
 
 // DIR
-define('DIR_APPLICATION', '/Users/mzaitsev/Workspace/shop/catalog/');
-define('DIR_SYSTEM', '/Users/mzaitsev/Workspace/shop/system/');
-define('DIR_IMAGE', '/Users/mzaitsev/Workspace/shop/image/');
-define('DIR_LANGUAGE', '/Users/mzaitsev/Workspace/shop/catalog/language/');
-define('DIR_TEMPLATE', '/Users/mzaitsev/Workspace/shop/catalog/view/theme/');
-define('DIR_CONFIG', '/Users/mzaitsev/Workspace/shop/system/config/');
-define('DIR_CACHE', '/Users/mzaitsev/Workspace/shop/system/storage/cache/');
-define('DIR_DOWNLOAD', '/Users/mzaitsev/Workspace/shop/system/storage/download/');
-define('DIR_LOGS', '/Users/mzaitsev/Workspace/shop/system/storage/logs/');
-define('DIR_MODIFICATION', '/Users/mzaitsev/Workspace/shop/system/storage/modification/');
-define('DIR_UPLOAD', '/Users/mzaitsev/Workspace/shop/system/storage/upload/');
+define('DIR_APPLICATION', '/Users/mzaitsev/Workspace/evadavidova/catalog/');
+define('DIR_SYSTEM', '/Users/mzaitsev/Workspace/evadavidova/system/');
+define('DIR_IMAGE', '/Users/mzaitsev/Workspace/evadavidova/image/');
+define('DIR_LANGUAGE', '/Users/mzaitsev/Workspace/evadavidova/catalog/language/');
+define('DIR_TEMPLATE', '/Users/mzaitsev/Workspace/evadavidova/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/mzaitsev/Workspace/evadavidova/system/config/');
+define('DIR_CACHE', '/Users/mzaitsev/Workspace/evadavidova/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Users/mzaitsev/Workspace/evadavidova/system/storage/download/');
+define('DIR_LOGS', '/Users/mzaitsev/Workspace/evadavidova/system/storage/logs/');
+define('DIR_MODIFICATION', '/Users/mzaitsev/Workspace/evadavidova/system/storage/modification/');
+define('DIR_UPLOAD', '/Users/mzaitsev/Workspace/evadavidova/system/storage/upload/');
 
 
 
